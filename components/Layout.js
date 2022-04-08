82a0121d6e295788faaf261f5aa27493989e88e7
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-08 18:02:11 +0530

    😎 fixing

--- components/Layout.js
+++ components/Layout.js
@@ -6,25 +6,37 @@ const StudentSideBar = dynamic(() => import('./StudentSideBar'))
 const Navbar = dynamic(() => import('./Navbar'))
 const ProfileRightSidebar = dynamic(() => import('./ProfileRightSidebar'))
 const SettingRightSideBar = dynamic(() => import('./SettingRightSideBar'))
+
 function Layout({ children }) {
-  const { route } = useRouter()
+  const { route } = useRouter();
+
   return (
     <div>
       <Navbar />
-      <div
-        className={`flex ${route.split('/')[1] === 'tutorDashboard' &&
-          'flex-col-reverse  bg-[#FFF2E3] lg:flex-row'
-          } `}
-      >
-        {route.split('/')[1] === 'tutorDashboard' && <SideBar />}
-        {route.split('/')[1] === 'studentDashboard' && <StudentSideBar />}
-        <main className="w-full flex-grow-0 overflow-x-auto">{children}</main>
-        {route.split('/')[2] === 'myprofile' ? (
-          <ProfileRightSidebar />
-        ) : route.split('/')[2] === 'settings' ? (
-          <SettingRightSideBar />
-        ) : null}
-      </div>
+      {route.split('/')[1] === 'tutorDashboard' && (
+        <div
+          className={`flex ${route.split('/')[1] === 'tutorDashboard' && 'flex-col-reverse  bg-[#FFF2E3] lg:flex-row'} `}>
+          {route.split('/')[1] === 'tutorDashboard' && <SideBar />}
+          <main className="w-full flex-grow-0 overflow-x-auto">{children}</main>
+          {route.split('/')[2] === 'myprofile' ? (
+            <ProfileRightSidebar />
+          ) : route.split('/')[2] === 'settings' ? (
+            <SettingRightSideBar />
+          ) : null}
+        </div>
+      )}
+      {route.split('/')[1] === 'studentDashboard' && (
+        <div
+          className={`flex ${route.split('/')[1] === 'studentDashboard' && 'flex-col-reverse  bg-[#FFF2E3] lg:flex-row'} `}>
+          {route.split('/')[1] === 'studentDashboard' && <StudentSideBar />}
+          <main className="w-full flex-grow-0 overflow-x-auto">{children}</main>
+          {route.split('/')[2] === 'myprofile' ? (
+            <ProfileRightSidebar />
+          ) : route.split('/')[2] === 'settings' ? (
+            <SettingRightSideBar />
+          ) : null}
+        </div>
+      )}
     </div>
   )
 }
