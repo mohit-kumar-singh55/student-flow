6ae89efafb4c9aa1cdae1b9cd3dcaac5d27bf404
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-09 11:26:33 +0530

    added all the icons

--- icons/Review.jsx
+++ icons/Review.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
 
-import React from 'react'
-import { AiOutlineStar } from 'react-icons/ai'
+export const ActiveReviewIcon = (props) => {
+  return (
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.5003 1.91602L14.4616 7.91518L21.0837 8.8831L16.292 13.5502L17.4228 20.1435L11.5003 17.0289L5.57783 20.1435L6.70866 13.5502L1.91699 8.8831L8.53908 7.91518L11.5003 1.91602Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  )
+}
 
 export const ReviewIcon = (props) => {
   return (
-    <div className="">
-      <AiOutlineStar className="h-6 w-6" {...props}/>
-    </div>
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.5003 1.91602L14.4616 7.91518L21.0837 8.8831L16.292 13.5502L17.4228 20.1435L11.5003 17.0289L5.57783 20.1435L6.70866 13.5502L1.91699 8.8831L8.53908 7.91518L11.5003 1.91602Z" stroke="#2C2C2C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+
   )
 }
